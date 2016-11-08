2766a1cfc21a570f106d22b25d62fd6f98be30da
Mike McCready <mikem@inspero.net>
2016-11-08 11:21:53 -0800

    create Track schema

---
+++ server/controllers/track-controller.js
@@ -0,0 +1,1 @@
+track-controller.js
--- server/server.js
+++ server/server.js
@@ -6,18 +6,34 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+// database connections
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/beatcreep-react');
 mongoose.connection.once('open', () => {
   console.log('connected to mongodb');
 });
 
+// middleware
 app.use(bodyParser.json());
 
+// serve index at root
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + './../index.html'));
 });
 
+// track api routes
+app.get('/api/tracks', (req, res) => {
+	console.log(req.body)
+});
+
+app.post('/api/tracks', (req, res) => {
+
+});
+
+
+// start server
 app.listen(PORT, () => {
 	console.log('server listening on port ', PORT)
 })
+
+module.exports = app;
