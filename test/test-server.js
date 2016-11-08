a62294fcb50e4c24fac416434def5f381eded932
Mike McCready <mikem@inspero.net>
2016-11-08 10:39:19 -0800

    add test for /api/tracks POST

--- test/test-server.js
+++ test/test-server.js
@@ -12,13 +12,40 @@ describe('Tracks', () => {
   		.get('/api/tracks')
   		.end((err, res) => {
   			res.should.have.status(200);
+  			res.should.be.json;
+  			res.body.should.be.a('array');
   			done();
   		});
   });
 
   it('should list a single track on /api/track/<id> GET');
 
-  it('should add a single track on /api/tracks POST');
+  it('should add a single track on /api/tracks POST', (done) => {
+  	chai.request(server)
+  		.post('/api/tracks')
+  		.send({'title': 'newSong', 'artist': 'newArtist', 'genre': 'db', 'plays': '0', 'favorited': 'false', 'source': 'beatport'})
+  		.end((err, res) => {
+  			res.should.have.status(200);
+  			res.should.be.json;
+  			res.body.should.be.a('object');
+  			res.body.should.have.property('SUCCESS');
+	      res.body.SUCCESS.should.be.a('object');
+	      res.body.SUCCESS.should.have.property('title');
+	      res.body.SUCCESS.should.have.property('artist');
+	      res.body.SUCCESS.should.have.property('genre');
+	      res.body.SUCCESS.should.have.property('plays');
+	      res.body.SUCCESS.should.have.property('favorited');
+	      res.body.SUCCESS.should.have.property('source');
+	      res.body.SUCCESS.should.have.property('_id');
+	      res.body.SUCCESS.title.should.equal('newSong');
+	      res.body.SUCCESS.artist.should.equal('newArtist');
+	      res.body.SUCCESS.genre.should.equal('db');
+	      res.body.SUCCESS.plays.should.equal('0');
+	      res.body.SUCCESS.favorited.should.equal('false');
+	      res.body.SUCCESS.source.should.equal('beatport');
+	      done();     	      
+  		})
+  });
 
   it('should update a single track on /api/track/<id> PUT');
 
