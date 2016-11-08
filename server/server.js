5674d53444bf70611b5cf1cb32803b4b5d2d7e85
Mike McCready <mikem@inspero.net>
2016-11-08 10:45:19 -0800

    initialize node server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
 // server.js
+const http = require('http');
+
+const PORT = 3000;
+
+const handleReq = (req, res) => {
+	res.end('reponding ', req.url)
+}
+
+const app = http.createServer(handleReq);
+
+app.listen(PORT, () => {
+	console.log('server listening on port ', PORT)
+})
