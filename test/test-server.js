513496001c2cc26d75bde0a847fbfd9bab27fe77
Mike McCready <mikem@inspero.net>
2016-11-08 10:09:14 -0800

    install mocha, initial server tests

---
+++ test/test-server.js
@@ -0,0 +1,8 @@
+// test-server.js
+describe('Tracks', () => {
+  it('should list all tracks on /api/tracks GET');
+  it('should list a single track on /api/track/<id> GET');
+  it('should add a single track on /api/tracks POST');
+  it('should update a single track on /api/track/<id> PUT');
+  it('should delete a single track on /api/track/<id> DELETE');
+});
