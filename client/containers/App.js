8c9ece76a608abf357d3a3cf4f366c24f1f8d59f
Mike McCready <mikem@inspero.net>
2016-11-09 18:27:43 -0800

    initialize App container, create secret file for soundcloud api

---
+++ client/containers/App.js
@@ -0,0 +1,17 @@
+// App.js
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+	render() {
+		return(
+			<div>
+				
+			</div>
+		)
+	}
+}
--- client/index.js
+++ client/index.js
@@ -2,7 +2,9 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import App from './containers/App';
+
 // attach point
 const mountPoint = document.getElementById('main-container');
 
-render((<div>hi</div>), mountPoint);
+render(<App />, mountPoint);
